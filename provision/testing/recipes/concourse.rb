5c14f551579d206162b53d0939f56d0caaa92e25
Robert Byrne <robert@byrnemail.org>
2016-10-11 17:58:32 +0100

    Concourse box

--- provision/testing/recipes/concourse.rb
+++ provision/testing/recipes/concourse.rb
@@ -1,8 +1,17 @@
 
+port=5432
+
 include_recipe 'debian-dev-box::postgres'
 
 remote_file '/usr/local/bin/concourse' do
     source 'https://github.com/concourse/concourse/releases/download/v2.2.1/concourse_linux_amd64' 
+    owner 'root'
+    mode '0755'
+    action :create
+end
+
+remote_file '/usr/local/bin/fly' do
+    source'http://localhost:8080/api/v1/cli?arch=amd64&platform=linux'
     owner 'root'
     mode '0755'
     action :create
@@ -20,4 +29,15 @@ execute 'worker_key' do
     cwd '/home/vagrant'
 end
 
+execute "create-user" do
+    command "createuser -U postgres -p #{port} -s $(whoami)"
+    not_if  %Q(psql -U postgres -p #{port} -tAc "select 1 from pg_roles where rolname='$(whoami)'" | grep 1)
+    user    "postgres"
+end
+
+execute "create-db" do
+    command "createdb -U postgres -p #{port} atc"
+    not_if  %Q(psql -U postgres -p #{port} -tAc "select 1 from pg_database where datname='atc'"| grep 1)
+    user    "postgres"
+end
 
