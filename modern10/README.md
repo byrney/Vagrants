b9f86d9b00c868123ae22bd7dd19fde018b94a7a
Robert Byrne <robert@byrnemail.org>
2017-06-15 17:22:47 +0100

    Add windows 10 box

---
+++ modern10/README.md
@@ -0,0 +1,194 @@
+# Modern IE Vagrant Setup with Chef #
+
+This machine is not meant to be used for anything it's simply
+a way of taking the ms provided vagrant boxes for IE testing and create
+a usable box that allows remote provisioning.
+
+The images provided by MS expire after 90 days so this process will have to be
+repeated.
+
+
+1. Install Prerequisites
+2. Get the Vagrant Box for IE Modern
+3. Prepare the guest
+4. Start up and provision with chef + vagrant
+
+## Prerequisites ##
+
+### Install Virtualbox ###
+
+
+### Install vagrant on the host ###
+
+Either download it from here
+
+    https://www.vagrantup.com/downloads
+
+or use you package manager of choice
+
+### Install chef dev kit on the host ###
+
+Download from opscode
+
+    https://downloads.chef.io/chef-dk/
+
+or, again, use you package manager
+
+### Install the vagrant-berkshelf-plugin ##
+
+Once vagrant is install this can be done with
+
+    vagrant plugin install vagrant-berkshelf
+
+on the commandline  (see   https://github.com/berkshelf/vagrant-berkshelf  for
+more details )
+
+## Get the Vagrant Box ##
+
+Clone this repo
+
+```bash
+git clone https://github.com/byrney/vagrants
+```
+
+change to the right directory and launch the box
+
+```bash
+    cd modern10
+    vagrant up
+```
+
+This will download the image from aka.ms and could take a couple of hours
+depending
+
+The box will start in the VirtualBox GUI but vagrant will fail because the box
+has not be set-up to allow remote access (see below for example of the error).
+
+You should see this:
+
+```
+> vagrant up
+
+Bringing machine 'default' up with 'virtualbox' provider...
+==> default: Clearing any previously set forwarded ports...
+==> default: Clearing any previously set network interfaces...
+==> default: Preparing network interfaces based on configuration...
+    default: Adapter 1: nat
+==> default: Forwarding ports...
+    default: 5985 => 5985 (adapter 1)
+    default: 3389 => 3389 (adapter 1)
+==> default: Booting VM...
+==> default: Waiting for machine to boot. This may take a few minutes...
+    default: WinRM address: 127.0.0.1:5985
+    default: WinRM username: vagrant
+    default: WinRM transport: plaintext
+An error occurred executing a remote WinRM command.
+
+Shell: powershell
+Command: hostname
+if ($?) { exit 0 } else { if($LASTEXITCODE) { exit $LASTEXITCODE } else { exit 1 } }
+Message: Protocol wrong type for socket
+```
+
+Go to the VM GUI and open a command window as admin (right click on the start
+button and select from the menu). Then
+
+```PowerShell
+powershell
+Set-ExecutionPolicy -executionpolicy remotesigned -force
+net use z: \\vboxsvr\vagrant
+. z:\vagrant_prepare.ps1
+```
+
+this should prep-the box for vagrant to be able to connect via winrm as user
+`vagrant`.
+
+## Configure your base box
+
+If you're going to be using this for 90 days then you want to get as much of
+the gui-only configuration into the base image. Things like
+
+* Apply as many windows updates as possible
+* Disable any unnecessary services
+* Turn off windows firewall
+* Turn off Windows Defender
+* Show file extensions in Explorer
+* Log in once as vagrant so the stock apps get set up
+* Set your language
+* Adjust privacy settings
+* ... and so on
+
+Let it reboot so that the updates get installed, then check for updates again
+(repeat until no more updates arrive)
+
+You can save yourself 400MB if you remove some components and optimize the
+drive in the image prior to export. There is a powershell script `Compact.ps1`
+that does this and tries to zero out as much of the unused space as possible.
+
+
+## Save the prepared image ##
+
+1.  Shut down the VM
+
+    vagrant halt
+
+2.  Back on the host, Export/Package the box
+
+    vagrant package --base modern10 --out modern10.box
+
+3.  Once the box is created import it
+
+    vagrant box add --name win10 --provider virtualbox  --force modern10.box
+
+This will create a new box `win10` that contains all your customizations
+
+You can delete the `modern10.box` file now.
+
+## Start up with vagrant ##
+
+You can now modify the Vagrantfile (or better, make a copy and modify that) to
+use `win10` box and it should complete without errors now.
+
+```bash
+vagrant up
+```
+
+When the prepare script has worked you will see this:
+
+```
+> vagrant up
+
+Bringing machine 'default' up with 'virtualbox' provider...
+==> default: Clearing any previously set forwarded ports...
+==> default: Clearing any previously set network interfaces...
+==> default: Preparing network interfaces based on configuration...
+    default: Adapter 1: nat
+==> default: Forwarding ports...
+    default: 5985 => 5985 (adapter 1)
+    default: 3389 => 3389 (adapter 1)
+==> default: Booting VM...
+==> default: Waiting for machine to boot. This may take a few minutes...
+    default: WinRM address: 127.0.0.1:5985
+    default: WinRM username: vagrant
+    default: WinRM transport: plaintext
+==> default: Machine booted and ready!
+==> default: Checking for guest additions in VM...
+    default: Guest Additions Version: 4.3.10
+    default: VirtualBox Version: 5.0
+==> default: Mounting shared folders...
+    default: /vagrant => /Users/rob/Documents/Vagrant/modern
+
+```
+
+## Repeat
+
+The `modern10` box registered with vagrant is the original ms image without
+activation so issuing
+
+    vagrant destroy -f && vagrant up
+
+in the modern8 folder will get you back to the beginning of this sequence so
+you can generate a new win81 after 90 days. This skips the download but, of
+course, the manual steps to configure the image will have to be repeated.
+
+
---
+++ modern10/vagrant_prepare.ps1
@@ -0,0 +1,190 @@
+function IsWinServer
+{
+    $osname = (Get-WMIObject Win32_OperatingSystem).Caption | Out-String
+    if($osname -match 'Server')
+    {
+        return $true;
+    }
+    return $false;
+}
+
+function UserExists([string] $username)
+{
+    $objOu = [ADSI]"WinNT://${env:ComputerName}"
+    $localUsers = $objOu.Children | where {$_.SchemaClassName -eq 'user'}  |  % {$_.name[0].ToString().ToLower()}
+    if($localUsers -contains $username.ToLower())
+    {
+        return $true;
+    }
+    return $false;
+}
+
+
+function Call([string]$title, $block)
+{
+    Write-Host '==== START ' $title
+    Try
+    {
+        &$block
+    }
+    Catch [Exception] {
+        Write-Host '==== ERROR ' $_.Exception.Message
+        pause
+    }
+    Write-Host '==== END ' $title
+}
+
+$isWinServer = IsWinServer
+
+# Powershell Script to prepare the windows install to be used with vagrant-windows
+
+Set-ExecutionPolicy -executionpolicy remotesigned -force
+
+# Step 1: Disable UAC
+Call 'Step 1: Disable UAC and GPO' {
+        New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\Policies\System -Name EnableLUA -PropertyType DWord -Value 0 -Force | Out-Null
+        New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\Policies\System -Name DisableGPO -PropertyType DWord -Value 1 -Force | Out-Null
+        Write-Host "User Access Control (UAC) and group policy (GPO) has been disabled." -ForegroundColor Green
+    }
+
+# Step 2: Disable IE ESC
+Call 'Step 2: Disable IE ESC' {
+        if($isWinServer -eq $true)
+        {
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 | Out-Null
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 | Out-Null
+            Stop-Process -Name Explorer | Out-Null
+            Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+        }
+        else
+        {
+            Write-Host 'Skip: Not a WinServer host'
+        }
+    }
+
+
+# Step 3: Disable the shutdown tracker
+# Reference: http://www.askvg.com/how-to-disable-remove-annoying-shutdown-event-tracker-in-windows-server-2003-2008/
+Call 'Step 3: Disable the shutdown tracker' {
+        If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability")) {
+          New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability"
+        }
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" -Name "ShutdownReasonOn" -PropertyType DWord -Value 0 -Force -ErrorAction continue
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" -Name "ShutdownReasonUI" -PropertyType DWord -Value 0 -Force -ErrorAction continue
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" -Name "ShutdownReasonOn" -Value 0
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Reliability" -Name "ShutdownReasonUI" -Value 0
+        Write-Host "Shutdown Tracker has been disabled." -ForegroundColor Green
+    }
+
+
+# Step 4: Disable Automatic Updates
+# Reference: http://www.benmorris.me/2012/05/1st-test-blog-post.html
+Call 'Step 4: Disable Automatic Updates' {
+        $AutoUpdate = (New-Object -com "Microsoft.Update.AutoUpdate").Settings
+        $AutoUpdate.NotificationLevel = 1
+        $AutoUpdate.Save()
+        Write-Host "Windows Update has been disabled." -ForegroundColor Green
+    }
+
+
+# Step 5: Disable Complex Passwords
+# Reference: http://vlasenko.org/2011/04/27/removing-password-complexity-requirements-from-windows-server-2008-core/
+Call 'Step 5: Disable Complex Passwords' {
+        $seccfg = [IO.Path]::GetTempFileName()
+        secedit /export /cfg $seccfg
+        (Get-Content $seccfg) | Foreach-Object {$_ -replace "PasswordComplexity\s*=\s*1", "PasswordComplexity=0"} | Set-Content $seccfg
+        secedit /configure /db $env:windir\security\new.sdb /cfg $seccfg /areas SECURITYPOLICY
+        del $seccfg
+        Write-Host "Complex Passwords have been disabled." -ForegroundColor Green
+    }
+
+# Step 6: Enable Remote Desktop
+# Reference: http://social.technet.microsoft.com/Forums/windowsserver/en-US/323d6bab-e3a9-4d9d-8fa8-dc4277be1729/enable-remote-desktop-connections-with-powershell
+Call 'Step 6: Enable Remote Desktop' {
+        set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0  
+        set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1 
+        Write-Host "Remote Desktop has been enabled" -ForegroundColor Green
+    }
+
+Call 'Step 6a: Private networking' {
+        # set the ethernet network to be private before attempting to activate winrm
+        get-netadapter | set-netconnectionprofile -NetworkCategory Private
+        Write-Host "All networks have been set to private" -ForegroundColor Green
+        }
+
+# Step 7: Enable WinRM Control
+Call 'Step 7: Enable WinRM Control' {
+        winrm quickconfig -q
+        winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="512"}' | out-null
+        winrm set winrm/config '@{MaxTimeoutms="1800000"}' | out-null
+        winrm set winrm/config/service '@{AllowUnencrypted="true"}' | out-null
+        winrm set winrm/config/service/auth '@{Basic="true"}' | out-null
+        winrm set winrm/config/listener?Address=*+Transport=HTTP @{Port="5985"} | out-null
+        # make winrm start auto  (no ps command for this)
+        invoke-expression "sc.exe config winrm start=auto"
+        Write-Host "WinRM has been configured and enabled." -ForegroundColor Green
+    }
+
+
+# Step 8: Disable Windows Firewall
+Call 'Step 8: Disable Windows Firewall' {
+        &netsh "advfirewall" "set" "allprofiles" "state" "off"
+        Write-Host "Windows Firewall has been disabled." -ForegroundColor Green
+    }
+
+# Step 9: Create local vagrant user
+Call 'Step 9: Create local vagrant user' {
+        $userexists = UserExists('vagrant')
+        if($userexists -eq $false)
+        {
+            $userDirectory = [ADSI]"WinNT://localhost"
+            $user = $userDirectory.Create("User", "vagrant")
+            $user.SetPassword("vagrant")
+            $user.SetInfo()
+            $user.UserFlags = 64 + 65536 # ADS_UF_PASSWD_CANT_CHANGE + ADS_UF_DONT_EXPIRE_PASSWD
+            $user.SetInfo()
+            $user.FullName = "vagrant"
+            $user.SetInfo()
+            &net "localgroup" "administrators" "/add" "vagrant"
+            Write-Host "User: 'vagrant' has been created as a local administrator." -ForegroundColor Green
+        }
+        else
+        {
+            Write-Host "vagrant user already exists."
+        }
+    }
+
+# Step 10: Create local vagrant user
+Call 'Step 10: Disable some bothersome services' {
+        invoke-expression "sc.exe config aelookupsvc start=disabled"
+        invoke-expression "sc.exe config pcasvc start=disabled"
+    }
+
+# Call 'Step 10: install chef client' {
+#     if(test-path "c:\opscode\chef\bin\chef-client") {
+#         write-host "Chef already installed"
+#         return
+#     }
+#     $chefUrl = "https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/chef-client-12.5.1-1-x86.msi"
+#     $tempDir = $env:Temp
+#     $file = Join-Path $tempDir "chef-install.msi"
+
+#     # download the package
+#     if(test-path $file){
+#         write-host "msi already downloaded at $file"
+#     } else {
+#       write-host "Downloading chef MSI from $chefUrl"
+#       $downloader = new-object System.Net.WebClient
+#       $downloader.Proxy.Credentials=[System.Net.CredentialCache]::DefaultNetworkCredentials;
+#       $downloader.DownloadFile($chefUrl, $file)
+#     }
+#     write-host "Installing chef msi $file"
+#     & cmd /c msiexec /qn /i "$file"
+#     Write-Host "Chef Client has been installed" -ForegroundColor Green
+# }
+
+Write-Host "Restarting Computer." -ForegroundColor Yellow
+
+pause
+
+Restart-Computer
