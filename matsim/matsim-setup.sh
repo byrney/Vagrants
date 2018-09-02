a981d1b72917f087a6211c988e5672378e5c33b6
Robert Byrne <robert@byrnemail.org>
2018-09-02 22:40:56 +0100

    Update matsim script
    * Tidy up a bit
    * Add some tools like nvim

--- matsim/matsim-setup.sh
+++ matsim/matsim-setup.sh
@@ -2,19 +2,19 @@
 
 set -euo pipefail
 
-readonly jdk='jdk1.8.0_181'
-readonly jdkdir="/usr/local/jdk"
-# http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
-readonly jdkurl="http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz"
-readonly matsim='matsim-0.10.1'
-readonly matsimurl="https://github.com/matsim-org/matsim/releases/download/$matsim/$matsim.zip"
-readonly matsimdir='/home/vagrant/'
-function install_tools(){
+
+function install_deps(){
     apt-get update && apt-get install -yq curl unzip xorg xfce4
 }
 
+function install_tools(){
+    apt-get update && apt-get install -yq neovim nano tmux ntp
+}
+
 function install_jdk(){
-    # curl -L -b "oraclelicense=a" -O 'http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jdk-10.0.2_linux-x64_bin.tar.gz'
+    declare -r jdk='jdk1.8.0_181'
+    declare -r jdkdir="/usr/local/jdk"
+    declare -r jdkurl="http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz"
     [[ -f "$HOME/$jdk.tgz" ]] || curl -sSL --fail -b "oraclelicense=a" -o "$HOME/$jdk.tgz" "$jdkurl"
     mkdir -p "$jdkdir"
     [[ -e "$jdkdir/$jdk/javac" ]] || tar zxf "$HOME/$jdk.tgz" -C "$jdkdir"
@@ -24,6 +24,9 @@ function install_jdk(){
 }
 
 function install_matsim(){
+    declare -r matsim='matsim-0.10.1'
+    declare -r matsimurl="https://github.com/matsim-org/matsim/releases/download/$matsim/$matsim.zip"
+    declare -r matsimdir='/home/vagrant/'
     [[ -f "$HOME/$matsim.zip" ]] || curl -sSL -o "$HOME/$matsim.zip"  "$matsimurl"
     [[ -d "$matsimdir/$matsim" ]] || unzip "$HOME/$matsim.zip" && chown -R vagrant:vagrant "/home/vagrant/$matsim"
 }
@@ -35,9 +38,9 @@ function install_via(){
     [[ -d "$via" ]] || unzip "$HOME/$via.zip" && chown -R vagrant:vagrant "/home/vagrant/$via"
 }
 
-
-install_tools
-install_jdk
-install_matsim
+install_deps && \
+install_tools && \
+install_jdk && \
+install_matsim && \
 install_via
 
